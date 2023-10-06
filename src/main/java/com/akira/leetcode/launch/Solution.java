9f27595d050b2d9ad938580b7d218970904216ac
taozhenkai <zk_tao@163.com>
2023-10-06 22:25:36 +0800

    :construction: longest-palindromic-substring

--- src/main/java/com/akira/leetcode/launch/Solution.java
+++ src/main/java/com/akira/leetcode/launch/Solution.java
@@ -1,9 +1,9 @@
 package com.akira.leetcode.launch;
 
-import com.akira.leetcode.longestsubstringwithoutrepeatingcharacters3.LongestSubstringWithoutRepeatingCharacters;
+import com.akira.leetcode.longestpalindromicsubstring5.LongestPalindromicSubstring;
 
 public class Solution {
     public static void main(String[] args) {
-        LongestSubstringWithoutRepeatingCharacters.launch();
+        LongestPalindromicSubstring.launch();
     }
 }
