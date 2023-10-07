dfe995c4700ac46baa9d6733e563c1e0c08353ce
taozhenkai <zk_tao@163.com>
2023-10-07 23:02:39 +0800

    :construction: quan-pai-lie

--- src/main/java/com/akira/leetcode/launch/Solution.java
+++ src/main/java/com/akira/leetcode/launch/Solution.java
@@ -1,9 +1,9 @@
 package com.akira.leetcode.launch;
 
-import com.akira.leetcode.longestpalindromicsubstring5.LongestPalindromicSubstring;
+import com.akira.leetcode.quanpailie46.QuanPaiLie;
 
 public class Solution {
     public static void main(String[] args) {
-        LongestPalindromicSubstring.launch();
+        QuanPaiLie.launch();
     }
 }
