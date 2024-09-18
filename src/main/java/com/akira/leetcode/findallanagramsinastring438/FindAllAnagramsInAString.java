6f92aceae07dc803879d44f11a2bbcbcb89ee633
ak1ra <zk_tao@163.com>
2024-09-18 10:24:22 +0800

    :construction: find-all-anagrams-in-a-string

--- src/main/java/com/akira/leetcode/findallanagramsinastring438/FindAllAnagramsInAString.java
+++ src/main/java/com/akira/leetcode/findallanagramsinastring438/FindAllAnagramsInAString.java
@@ -17,7 +17,7 @@ public class FindAllAnagramsInAString {
 
     public static void launch() {
         FindAllAnagramsInAString solution = new FindAllAnagramsInAString();
-        List<Integer> ans = solution.findAnagrams("cbaebabacd", "cba");
+        List<Integer> ans = solution.findAnagramsAdvance("cbaebabacd", "cba");
         ans.forEach(System.out::println);
     }
 
@@ -64,6 +64,54 @@ public class FindAllAnagramsInAString {
             //判断滑动后处，是否有异位词
             if(Arrays.equals(scount,pcount)){
                 ans.add(i+1);
+            }
+        }
+
+        return ans;
+    }
+
+    public List<Integer> findAnagramsAdvance(String s, String p) {
+        int sLen = s.length(), pLen = p.length();
+
+        if (sLen < pLen) {
+            return new ArrayList<Integer>();
+        }
+
+        List<Integer> ans = new ArrayList<Integer>();
+        int[] count = new int[26];
+        for (int i = 0; i < pLen; ++i) {
+            ++count[s.charAt(i) - 'a'];
+            --count[p.charAt(i) - 'a'];
+        }
+
+        int differ = 0;
+        for (int j = 0; j < 26; ++j) {
+            if (count[j] != 0) {
+                ++differ;
+            }
+        }
+
+        if (differ == 0) {
+            ans.add(0);
+        }
+
+        for (int i = 0; i < sLen - pLen; ++i) {
+            if (count[s.charAt(i) - 'a'] == 1) {  // 窗口中字母 s[i] 的数量与字符串 p 中的数量从不同变得相同
+                --differ;
+            } else if (count[s.charAt(i) - 'a'] == 0) {  // 窗口中字母 s[i] 的数量与字符串 p 中的数量从相同变得不同
+                ++differ;
+            }
+            --count[s.charAt(i) - 'a'];
+
+            if (count[s.charAt(i + pLen) - 'a'] == -1) {  // 窗口中字母 s[i+pLen] 的数量与字符串 p 中的数量从不同变得相同
+                --differ;
+            } else if (count[s.charAt(i + pLen) - 'a'] == 0) {  // 窗口中字母 s[i+pLen] 的数量与字符串 p 中的数量从相同变得不同
+                ++differ;
+            }
+            ++count[s.charAt(i + pLen) - 'a'];
+
+            if (differ == 0) {
+                ans.add(i + 1);
             }
         }
 
