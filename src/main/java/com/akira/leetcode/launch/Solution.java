653aa1bce52acce1995851d6d9ebb41371c1833a
ak1ra <zk_tao@163.com>
2024-12-17 11:30:19 +0800

    :poop: LongestIncreasingSubsequence Using Greedy And HalfSearch

--- src/main/java/com/akira/leetcode/launch/Solution.java
+++ src/main/java/com/akira/leetcode/launch/Solution.java
@@ -1,11 +1,11 @@
 package com.akira.leetcode.launch;
 
-import com.akira.leetcode.wordbreak139.WordBreak;
+import com.akira.leetcode.longestincreasingsubsequence300.LongestIncreasingSubsequence;
 
 public class Solution {
     public static void main(String[] args) {
         long startTime = System.currentTimeMillis();
-        WordBreak.launch();
+        LongestIncreasingSubsequence.launch();
         long endTime = System.currentTimeMillis();
         System.out.println("耗时：" + (endTime - startTime) + "ms");
     }
--- src/main/java/com/akira/leetcode/longestincreasingsubsequence300/LongestIncreasingSubsequence.java
+++ src/main/java/com/akira/leetcode/longestincreasingsubsequence300/LongestIncreasingSubsequence.java
@@ -3,7 +3,7 @@ package com.akira.leetcode.longestincreasingsubsequence300;
 public class LongestIncreasingSubsequence {
     public static void launch() {
         LongestIncreasingSubsequence solution = new LongestIncreasingSubsequence();
-        System.out.println(solution.lengthOfLIS(new int[]{10,9,2,5,3,7,101,18}));
+        System.out.println(solution.lengthOfLISUsingGreedyHalf(new int[]{10,9,2,5,3,7,101,18}));
     }
 
     public int lengthOfLIS(int[] nums) {
@@ -25,8 +25,39 @@ public class LongestIncreasingSubsequence {
         return max;
     }
 
-    // todo: 贪心+二分查找
+    /**
+     * official solution ：贪心+二分查找
+     * <a href = https://writings.sh/post/longest-increasing-subsequence-revisited />
+     * @param nums
+     * @return int
+     * @author tzk
+     * @date 2024/12/17 11:21
+     * @description
+     */
     public int lengthOfLISUsingGreedyHalf(int[] nums) {
-        return 0;
+        int len = 1, n = nums.length;
+        if (n == 0) {
+            return 0;
+        }
+        int[] d = new int[n + 1];
+        d[len] = nums[0];
+        for (int i = 1; i < n; ++i) {
+            if (nums[i] > d[len]) {
+                d[++len] = nums[i];
+            } else {
+                int l = 1, r = len, pos = 0; // 如果找不到说明所有的数都比 nums[i] 大，此时要更新 d[1]，所以这里将 pos 设为 0
+                while (l <= r) {
+                    int mid = (l + r) >> 1;
+                    if (d[mid] < nums[i]) {
+                        pos = mid;
+                        l = mid + 1;
+                    } else {
+                        r = mid - 1;
+                    }
+                }
+                d[pos + 1] = nums[i];
+            }
+        }
+        return len;
     }
 }
