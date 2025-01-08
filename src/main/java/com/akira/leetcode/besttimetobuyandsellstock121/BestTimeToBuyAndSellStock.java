76e55abbf8dd94e1b911de6b6bc45b34709afc61
ak1ra <zk_tao@163.com>
2025-01-08 16:26:10 +0800

    :memo: Add some notes for better understanding of the solution on BestTimeToBuyAndSellStock

--- src/main/java/com/akira/leetcode/besttimetobuyandsellstock121/BestTimeToBuyAndSellStock.java
+++ src/main/java/com/akira/leetcode/besttimetobuyandsellstock121/BestTimeToBuyAndSellStock.java
@@ -7,7 +7,14 @@ public class BestTimeToBuyAndSellStock {
         System.out.println(profit);
     }
 
-    // O(n2) 解法
+    /**
+     * 暴力解法：超时
+     * @param prices
+     * @return int
+     * @author Ak1ra
+     * @date 2025/1/8 11:28
+     * @description
+     */
     public int maxProfitN2(int[] prices) {
         int maxProfit = 0;
         if (prices.length > 0) {
@@ -24,7 +31,7 @@ public class BestTimeToBuyAndSellStock {
 
     /**
      * 如果当前i下标对应的就是最小值，那么说明第i天卖出无法获取利润，直接不卖，利润是0
-     * 如果当前i下标对应的不是最小值，那么说明可以在前i-1天以最低成本去买入股票，然后第i天卖出，利润=第i天价格-最低成本
+     * 如果当前i下标对应的不是最小值，那么说明可以在前i-1天以最低成本去买入股票，然后第i天卖出，利润=第i天价格-最低成本[通过遍历是已知的]
      * 然后遍历，从局部利润，算出全局最大利润
      */
     public int maxProfit(int[] prices) {
@@ -32,8 +39,10 @@ public class BestTimeToBuyAndSellStock {
         int maxprofit = 0;
         for (int i = 0; i < prices.length; i++) {
             if (prices[i] < minprice) {
+                // 求出到此时的最优购买价格
                 minprice = prices[i];
             } else if (prices[i] - minprice > maxprofit) {
+                // 此刻卖出的利润属于最大利润
                 maxprofit = prices[i] - minprice;
             }
         }
