ebe20a09ed4aca051b1a3760e5a53f21d48ca346
ak1ra <zk_tao@163.com>
2025-01-16 13:53:55 +0800

    :construction: SpiralMatrix

--- src/main/java/com/akira/leetcode/launch/Solution.java
+++ src/main/java/com/akira/leetcode/launch/Solution.java
@@ -1,11 +1,11 @@
 package com.akira.leetcode.launch;
 
-import com.akira.leetcode.setmatrixzeroes73.SetMatrixZeroes;
+import com.akira.leetcode.spiralmatrix54.SpiralMatrix;
 
 public class Solution {
     public static void main(String[] args) {
         long startTime = System.currentTimeMillis();
-        SetMatrixZeroes.launch();
+        SpiralMatrix.launch();
         long endTime = System.currentTimeMillis();
         System.out.println("耗时：" + (endTime - startTime) + "ms");
     }
